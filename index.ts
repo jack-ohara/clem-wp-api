c12d81f87ccd0bd174c584dc55296af91b306069
Jack O'Hara <jack.ohara@infinityworks.com>
2022-03-28 21:04:54 +0100

    Error logging

--- index.ts
+++ index.ts
@@ -6,22 +6,31 @@ export async function handler(event: APIGatewayProxyEventV2): Promise<APIGateway
   const path = event.rawPath.split('/').slice(-1)[0]
   let result: unknown
 
-  switch (path) {
-    case 'posts':
-      result = await getPosts()
-      break
+  try {
+    switch (path) {
+      case 'posts':
+        result = await getPosts()
+        break
 
-    case 'pages':
-      result = await getPosts()
-      break
+      case 'pages':
+        result = await getPosts()
+        break
 
-    default:
-      result = {message: 'What the hell is that?'}
-      break
-  }
+      default:
+        result = { message: 'What the hell is that?' }
+        break
+    }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    }
+  } catch (error) {
+    console.error(JSON.stringify(error))
+
+    return {
+      statusCode: 500,
+      body: "Something went wrong... More info in logs"
+    }
   }
 }
