32893cd35bdb40ecbc7f83177c6b11d90eded323
Jack O'Hara <jack.ohara@infinityworks.com>
2022-03-28 20:47:24 +0100

    Read the path and setup some basic stuff depending on the value

--- index.ts
+++ index.ts
@@ -1,22 +1,28 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import NodeCache from "node-cache";
 
 const cache = new NodeCache({ stdTTL: 0 })
 
-export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> {
-  console.log(JSON.stringify(event, null, 2))
+export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+  const path = event.rawPath.split('/').slice(-1)[0]
+  let body: string
 
-  if (cache.has('keykey')) {
-    return {
-      statusCode: 200,
-      body: 'Hey from clem'
-    }
+  switch (path) {
+    case 'posts':
+      body = 'Here are some posts...'
+      break
+
+    case 'pages':
+      body = 'Here are some pages...'
+      break
+
+    default:
+      body = 'What the hell is that?'
+      break
   }
-
-  cache.set('keykey', { something: 'nothing' })
 
   return {
     statusCode: 200,
-    body: 'No dice'
+    body
   }
 }
