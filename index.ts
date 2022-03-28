68ad5bebff745456219eeda96f437c782ccb87a1
Jack O'Hara <jack.ohara@infinityworks.com>
2022-03-28 20:17:12 +0100

    Test for node-cache

--- index.ts
+++ index.ts
@@ -1,8 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 0 })
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> {
+  if (cache.has('keykey')) {
+    return {
+      statusCode: 200,
+      body: 'Hey from clem'
+    }
+  }
+
+  cache.set('keykey', { something: 'nothing' })
+
   return {
     statusCode: 200,
-    body: 'Hey from clem'
+    body: 'No dice'
   }
 }
