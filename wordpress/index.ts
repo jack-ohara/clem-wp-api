1c460153a866721465ca27ec7d42aafd81db998e
Jack O'Hara <jack.ohara@infinityworks.com>
2022-04-03 18:40:53 +0100

    Some logging

--- wordpress/index.ts
+++ wordpress/index.ts
@@ -289,7 +289,11 @@ async function fetchFromWordpress(relativeURL: string, retryCount: number = 5): 
   console.log(`Calling ${url}`)
 
   try {
-    return await fetch(url)
+    const result =  await fetch(url)
+
+    console.log(result)
+
+    return result
   } catch (e) {
     console.error(JSON.stringify(e, null, 2))
     console.log(url)
